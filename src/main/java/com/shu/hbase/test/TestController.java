57c9d6bd070b0ba899bd0e8930572d1442376190
chenxy@shaiic.com <2500941316@qq.com>
2020-09-23 14:58:38 +0800

    0.8test1

--- src/main/java/com/shu/hbase/test/TestController.java
+++ src/main/java/com/shu/hbase/test/TestController.java
@@ -1,6 +1,8 @@
 package com.shu.hbase.test;
 
 import com.shu.hbase.tools.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("testPublicFile")
     public String testPublicFile() {
 
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 15; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    logger.info("新的线程启动了"+Thread.currentThread().getName());
                     final String url = "http://localhost:8080/getPublicFiles";
                     String string = Get.sendGet(url, "", "");
                     System.out.println(string);
