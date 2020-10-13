4331e30f395900c04808e3788ac362e91328315c
chenxy@shaiic.com <2500941316@qq.com>
2020-10-13 19:34:49 +0800

    1.1分组

--- src/main/java/com/shu/hbase/tools/fastdfs/FastDFSClient.java
+++ src/main/java/com/shu/hbase/tools/fastdfs/FastDFSClient.java
@@ -19,13 +19,9 @@ public class FastDFSClient {
         try {
             // 获取配置文件
             logger.info("获取配置文件根路径");
-            ClassPathResource classPathResource = new ClassPathResource("conf/fdfs_client.conf");
-            String CONF_FILENAME = classPathResource.getPath();
-            System.out.println(classPathResource.getPath());
-            //String CONF_FILENAME = new File(FastDFSClient.class.getResource("/").getFile()).getCanonicalPath();
-            System.out.println(CONF_FILENAME);
+           // String CONF_FILENAME = new File(FastDFSClient.class.getResource("/").getFile()).getCanonicalPath();
             logger.info("拼接配置文件路径");
-           // String CONF_FILENAME = classPath + File.separator + "conf" + File.separator + "fdfs_client.conf";
+            String CONF_FILENAME = "/usr/local/springboot"+ File.separator + "fdfs_client.conf";
             ClientGlobal.init(CONF_FILENAME);
             logger.info("fdfs连接初始化成功");
             // 获取触发器
