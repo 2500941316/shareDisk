ce2b593333b0554ca8e6bb9edfbcb7b06f68f99f
chenxy@shaiic.com <2500941316@qq.com>
2020-10-14 16:37:24 +0800

    1.1分组

--- src/main/java/com/shu/hbase/test/UrlTestController.java
+++ src/main/java/com/shu/hbase/test/UrlTestController.java
@@ -1,6 +1,10 @@
 package com.shu.hbase.test;
 
+import com.shu.hbase.tools.hdfspool.HdfsConnectionPool;
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
@@ -23,17 +28,11 @@ public class UrlTestController {
     @GetMapping("testUrl")
     public void testUrl() {
         try {
-            logger.info("注册fdfs");
-            URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());//注册hdfs的URL
-            logger.info("开始获得文件输入流");
-            InputStream inputStream = new URL("hdfs://bdg-proto-nameservice/shuwebfs/00000000/上海大学奖学金评选申请0.docx").openStream();//获取文件输入流
-            logger.info("成功获得inputStream");
-            FileOutputStream outputStream = new FileOutputStream(new File("/home/file1_out.txt"));
-            logger.info("打开outputStream");
-            logger.info("开始拷贝");
-            IOUtils.copy(inputStream, outputStream);//实现文件的拷贝
-            IOUtils.closeQuietly(inputStream);//关闭流
-            IOUtils.closeQuietly(outputStream);
+            String target = "/shuwebfs/00000000/上海大学奖学金评选申请0.docx";//hdfs文件 地址
+            FileSystem hdfsConnection = HdfsConnectionPool.getHdfsConnection();
+            logger.info("获取到fs连接");
+            hdfsConnection.copyToLocalFile(new Path(target), new Path("/home/testHdfs.docx"));
+            logger.info("文件拷贝成功");
         } catch (Exception e) {
             e.printStackTrace();
         }
