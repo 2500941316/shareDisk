94302ad139ca730617cdce791ecca6cde339e6db
chenxy@shaiic.com <2500941316@qq.com>
2020-10-14 16:15:05 +0800

    1.1分组

--- src/main/java/com/shu/hbase/test/UrlTestController.java
+++ src/main/java/com/shu/hbase/test/UrlTestController.java
@@ -17,13 +17,17 @@ import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 public class UrlTestController {
 
     @GetMapping("testUrl")
-    public void testUrl() throws Exception {
-        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());//注册hdfs的URL
-        InputStream inputStream = new URL("hdfs://bdg-proto-nameservice/shuwebfs/00000000/上海大学奖学金评选申请0.docx").openStream();//获取文件输入流
-        FileOutputStream outputStream = new FileOutputStream(new File("/home/file1_out.txt"));
-        IOUtils.copy(inputStream, outputStream);//实现文件的拷贝
-        IOUtils.closeQuietly(inputStream);//关闭流
-        IOUtils.closeQuietly(outputStream);
+    public void testUrl() {
+        try {
 
+            URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());//注册hdfs的URL
+            InputStream inputStream = new URL("hdfs://bdg-proto-nameservice/shuwebfs/00000000/上海大学奖学金评选申请0.docx").openStream();//获取文件输入流
+            FileOutputStream outputStream = new FileOutputStream(new File("/home/file1_out.txt"));
+            IOUtils.copy(inputStream, outputStream);//实现文件的拷贝
+            IOUtils.closeQuietly(inputStream);//关闭流
+            IOUtils.closeQuietly(outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
